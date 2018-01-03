c417eec4a45b17a3ac9520141bbccc585ff0dbf9
Ian Roberts <ianroberts131@gmail.com>
2018-01-02 20:33:55 -0800

    format-header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -25,9 +25,14 @@ class Header extends React.Component {
   render() {
 
     return (
-      <div>
-        <h1>{this.state.artistName}</h1>
-        <img src={this.state.artistImage}/>
+      <div id="header-container">
+        <img id="artist-image" src={this.state.artistImage}/>
+        <div id="header-text-section">
+          <div id="header-text-wrapper">
+            <h2 id="artist-name">{this.state.artistName}</h2>
+            <h2 id="upcoming-events-header">Upcoming Events</h2>
+          </div>
+        </div>
       </div>
     )
 
