cbf7099ad0926ce61e9675fbbf514e706b7ee556
Ian Roberts <ianroberts131@gmail.com>
2018-01-03 05:54:32 -0800

    Display no upcoming events message if there are no scheduled events

---
+++ src/components/noEvents/NoEvents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './noEvents.css';
+
+class NoEvents extends React.Component {
+  render() {
+    return (
+      <div id="no-events">
+        No upcoming events.
+      </div>
+    )
+  }
+}
+
+export default NoEvents;
