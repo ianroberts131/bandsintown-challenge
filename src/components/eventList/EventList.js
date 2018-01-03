687ee50e3ed8d6fb28669986aa8614b273a99525
Ian Roberts <ianroberts131@gmail.com>
2018-01-02 21:37:02 -0800

    Conditionally display tickets if offer exists only

--- src/components/eventList/EventList.js
+++ src/components/eventList/EventList.js
@@ -6,7 +6,7 @@ class EventList extends React.Component {
   constructor() {
     super();
     this.state = {
-      events: {}
+      events: []
     }
   }
 
@@ -15,7 +15,7 @@ class EventList extends React.Component {
     .then(results => {
       return results.json();
     }).then(data => {
-      this.setState({ events: data})
+      this.setState({ events: data })
     })
   }
 
@@ -23,28 +23,25 @@ class EventList extends React.Component {
 
     return (
       <div id="events-container">
-        { Object.keys(this.state.events).map((key, index) => {
-          var event = this.state.events[key];
+        { this.state.events.map((event) => {
           var region = event.venue.country === "United States" ?
             event.venue.region : event.venue.country;
           var location = event.venue.city + ", " + region;
           var date = moment(event.datetime).format("MMM D");
-          var url = event.offers[0].url
+          var url = event.offers.length > 0 ? event.offers[0].url : "no ticket";
+          var ticketsId = url === "no ticket" ? "hide-ticket" : "tickets-button-wrapper";
           return (
             <div className="event">
               <div className="event-item" id="date-wrapper">
                 <span id="date">{ date }</span>
               </div>
-
               <div className="event-item" id="venue-wrapper">
                 <span>{event.venue.name}</span>
               </div>
-
               <div className="event-item" id="location-wrapper">
                 <span>{ location }</span>
               </div>
-
-              <div className="event-item" id="tickets-button-wrapper">
+              <div className="event-item" id={ ticketsId }>
                 <a id="tickets-button" href={ url }>Tickets</a>
               </div>
             </div>
