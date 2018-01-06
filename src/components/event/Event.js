f529054789e0fffadde06b30f13e7f06ad71a356
Ian Roberts <ianroberts131@gmail.com>
2018-01-06 10:19:34 -0800

    complete basic mobile layout

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -6,25 +6,31 @@ class Event extends React.Component {
     const { event, date, venue, location } = this.props;
 
     var url = event.offers.length > 0 ? event.offers[0].url : "no ticket";
-    var ticketsId = url === "no ticket" ? "hide-ticket" : "tickets-button-wrapper";
+    var ticketsId = url === "no ticket" ? "hide-ticket" : "tickets-button-section";
 
     return (
       <div className="event">
 
-        <div className="event-item" id="date-wrapper">
-          <span id="date">{ date }</span>
-        </div>
+        <div id="event-information-section">
+          <div id="event-information-wrapper">
+            <div className="event-item" id="date-wrapper">
+              <span id="date">{ date }</span>
+            </div>
 
-        <div className="event-item" id="venue-wrapper">
-          <span>{ venue }</span>
-        </div>
+            <div className="event-item" id="venue-wrapper">
+              <span>{ venue }</span>
+            </div>
 
-        <div className="event-item" id="location-wrapper">
-          <span>{ location }</span>
+            <div className="event-item" id="location-wrapper">
+              <span>{ location }</span>
+            </div>
+          </div>
         </div>
 
         <div className="event-item" id={ ticketsId }>
-          <a id="tickets-button" href={ url }>Tickets</a>
+          <div id="tickets-button-wrapper">
+            <a id="tickets-button" href={ url }>Tickets</a>
+          </div>
         </div>
 
       </div>
